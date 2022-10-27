e1ac73d5d5d0b2e63d28103b8a11cb5e072758f0
failall <KlemensHofmann24@gmail.com>
2022-10-27 22:52:01 +0200

    updated logic providers for CultivationRealm

--- src/main/java/com/failall/cultivation_miracle/util/CultivationLogicProviders.java
+++ src/main/java/com/failall/cultivation_miracle/util/CultivationLogicProviders.java
@@ -1,6 +1,8 @@
 package com.failall.cultivation_miracle.util;
 
+import com.failall.cultivation_miracle.cultivation.CultivationRealm;
 import com.failall.cultivation_miracle.cultivation.CultivationStage;
+import com.failall.cultivation_miracle.registry.RegistryCultivationRealms;
 import com.failall.cultivation_miracle.registry.RegistryCultivationStages;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.registries.RegistryObject;
@@ -14,11 +16,30 @@ public class CultivationLogicProviders {
     }
 
     // This methods returns the RegistryObject of the given cultivation stage parameter that has a 1 higher internal ID
-    // (next realm = ID + 1)
-    public static RegistryObject<CultivationStage> getStageRegistryObjFromRegistry(CultivationStage cultivationStage) {
+    // (next stage = ID + 1)
+    public static RegistryObject<CultivationStage> getNewStageRegistryObjFromRegistry(CultivationStage cultivationStage) {
         return RegistryCultivationStages.DEFERRED_CULTIVATION_STAGES.getEntries().stream().filter(
                 c -> c.get().getStageRankingNumber() == cultivationStage.getStageRankingNumber() + 1
+        ).filter(c -> c.get().getRealm().getRealmRankingNumber() ==
+                getRealmRegistryObjFromRegistry(cultivationStage.getRealm()).get().getRealmRankingNumber()).findFirst().get();
+    }
+
+    public static RegistryObject<CultivationRealm> getNewRealmRegistryObjFromRegistry(CultivationRealm cultivationRealm) {
+        return RegistryCultivationRealms.DEFERRED_CULTIVATION_REALMS.getEntries().stream().filter(
+                c -> c.get().getRealmRankingNumber() == cultivationRealm.getRealmRankingNumber() + 1
         ).findFirst().get();
+    }
+
+    public static RegistryObject<CultivationRealm> getRealmRegistryObjFromRegistry(CultivationRealm cultivationRealm) {
+        return RegistryCultivationRealms.DEFERRED_CULTIVATION_REALMS.getEntries().stream().filter(
+                c -> c.get().getRealmRankingNumber() == cultivationRealm.getRealmRankingNumber()
+        ).findFirst().get();
+    }
+
+    public static CultivationRealm getCultivationRealmFromRegistry(ResourceLocation resourceLocation) {
+        return RegistryCultivationRealms.DEFERRED_CULTIVATION_REALMS.getEntries().stream().filter(
+                c -> c.getId() == resourceLocation
+        ).findFirst().get().get();
     }
 
 }
