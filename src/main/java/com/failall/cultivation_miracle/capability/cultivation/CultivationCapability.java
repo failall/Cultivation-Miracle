799e770e210053d70202ac6e2503f382f067650b
failall <KlemensHofmann24@gmail.com>
2022-10-27 19:40:50 +0200

    Added logic for breakthrough through stage Re-assign new attribute values and stats

--- src/main/java/com/failall/cultivation_miracle/capability/cultivation/CultivationCapability.java
+++ src/main/java/com/failall/cultivation_miracle/capability/cultivation/CultivationCapability.java
@@ -1,17 +1,49 @@
 package com.failall.cultivation_miracle.capability.cultivation;
 
+import com.failall.cultivation_miracle.CultivationMiracle;
+import com.failall.cultivation_miracle.cultivation.CultivationStage;
+import com.failall.cultivation_miracle.registry.RegistryCultivationRealms;
+import com.failall.cultivation_miracle.registry.RegistryCultivationStages;
+import com.failall.cultivation_miracle.util.CultivationLogicProviders;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class CultivationCapability {
 
     private int qi;
     private int maxQi;
+    private int thresholdQi;
+    private int maxQiFromCultivationStage;
     private int cultivationSpeed;
     private boolean isCultivating;
 
-    public CultivationCapability(int maxQi, int cultivationSpeed) {
+    private ResourceLocation cultivationRealm;
+    private ResourceLocation cultivationStage;
+
+    private HashMap<String,Double> playerAttr = new HashMap<>();
+
+    public CultivationCapability(int maxQi, int cultivationSpeed, int thresholdQi) {
+        this.qi = 0;
         this.maxQi = maxQi;
         this.cultivationSpeed = cultivationSpeed;
+        this.thresholdQi = thresholdQi;
+        this.cultivationRealm = RegistryCultivationRealms.PERCEPTION.getId();
+        initPlayerBaseAttributes();
+        //TODO add stage to constructor
+        this.cultivationStage = RegistryCultivationStages.PERCEPTION_I.getId();
+        this.cultivationRealm = RegistryCultivationRealms.ESTABLISHMENT.getId();
+    }
+
+    public ResourceLocation getCultivationRealm() {
+        return cultivationRealm;
+    }
+
+    public ResourceLocation getCultivationStage() {
+        return cultivationStage;
     }
 
     public int getCurrentQi() {
@@ -30,11 +62,9 @@ public class CultivationCapability {
     public int getMaxQi() {
         return maxQi;
     }
-    
-    public void setMaxQi(int maxQi) {
-        this.maxQi = maxQi;
-    }
 
+    // No Setter, cause when the hell would i ever want to set the maxQi to smth? What if i add maxQi through another process than lvl up, e.g.
+    // cultivating, items, potions, etc.?
     public void addMaxQi(int maxQiToAdd) {
         this.maxQi += maxQiToAdd;
     }
@@ -59,6 +89,26 @@ public class CultivationCapability {
         this.isCultivating = isCultivating;
     }
 
+    public void setPlayerAttr(String key, Double val) {
+        if (playerAttr.containsKey(key)) {
+            playerAttr.put(key, val);
+            return;
+        }
+
+        throw new ArrayIndexOutOfBoundsException();
+    }
+    public Double getPlayerAttr(String key) {
+        if (playerAttr.containsKey(key)) return playerAttr.get(key);
+
+        throw new ArrayIndexOutOfBoundsException();
+    }
+
+
+    //TODO Perform this method on rank up to add max QI depending on cultivation stage
+    public void setMaxQiFromCultivationStage(int maxQiFromCultivationStage) {
+        this.maxQiFromCultivationStage = maxQiFromCultivationStage;
+    }
+
 
 
     // REMEMBER if more capability features(e.g. cultivation realm/level) is added, add it to the copyFrom method
@@ -67,16 +117,29 @@ public class CultivationCapability {
         this.maxQi = source.maxQi;
         this.cultivationSpeed = source.cultivationSpeed;
         this.isCultivating = source.isCultivating;
+        this.maxQiFromCultivationStage = source.maxQiFromCultivationStage;
+        this.cultivationRealm = source.cultivationRealm;
+        this.playerAttr = source.playerAttr;
     }
 
     // see https://gist.github.com/TheCurle/6db954d680f6f067dcdc791355c32c89
-    // dont forget to implement those 2 methods so data will be saved to disk
+    // don't forget to implement those 2 methods so data will be saved to disk
     
     public void serializeNBT(CompoundTag tag) {
         tag.putInt("total_qi", this.qi);
         tag.putInt("max_qi", this.maxQi);
         tag.putInt("cultivation_speed", this.cultivationSpeed);
+        tag.putInt("threshold_qi", this.thresholdQi);
         tag.putBoolean("isCultivating", this.isCultivating);
+        tag.putInt("maxQiFromCultivationStage", this.maxQiFromCultivationStage);
+        tag.putString("cultivation_realm", this.cultivationRealm.getPath());
+        tag.putString("cultivation_stage", this.cultivationStage.getPath());
+        // Stats
+        CompoundTag stats = new CompoundTag();
+        for (var stat : this.playerAttr.keySet()) {
+            stats.putDouble(stat, this.playerAttr.get(stat));
+        }
+        tag.put("playerStats", stats);
     }
 
     
@@ -84,6 +147,63 @@ public class CultivationCapability {
         this.qi = nbt.getInt("total_qi");
         this.maxQi = nbt.getInt("max_qi");
         this.cultivationSpeed = nbt.getInt("cultivation_speed");
+        this.thresholdQi = nbt.getInt("threshold_qi");
         this.isCultivating = nbt.getBoolean("isCultivating");
+        this.maxQiFromCultivationStage = nbt.getInt("maxQiFromCultivationStage");
+        this.cultivationRealm = new ResourceLocation(CultivationMiracle.MOD_ID, nbt.getString("cultivation_realm"));
+        this.cultivationStage = new ResourceLocation(CultivationMiracle.MOD_ID, nbt.getString("cultivation_stage"));
+        // Stats
+        CompoundTag stats = nbt.getCompound("playerStats");
+        for (var stat : this.playerAttr.keySet()) {
+            this.playerAttr.put(stat, stats.getDouble(stat));
+        }
+    }
+
+    private void initPlayerBaseAttributes(){
+        playerAttr.put("MAX_HP",1d); // "MAX_HP"
+        playerAttr.put("DAMAGE",1d); // "DAMAGE"
+        playerAttr.put("ARMOR",0d); // "ARMOR"
+        // More to follow TODO also update this in CultivationStage.java
+    }
+
+    public void rankUpStage() {
+        final CultivationStage currentStage = CultivationLogicProviders.getCultivationStageFromRegistry(this.cultivationStage);
+
+        if (currentStage.getStageRankingNumber() == 7) {
+            rankUpRealm();
+            return;
+        }
+
+        RegistryObject<CultivationStage> newStage = CultivationLogicProviders.getStageRegistryObjFromRegistry(currentStage);
+
+        this.cultivationStage = newStage.getId();
+        this.maxQi += newStage.get().getMaxQiToAdd();
+        this.thresholdQi += newStage.get().getThresholdQiToAdd();
+        this.cultivationSpeed = newStage.get().getCultivationSpeedToAdd();
+        this.qi = this.qi / 2;
+    }
+
+    public void rankUpRealm() {
+
+    }
+
+    public void applyAttributeChangesFromStage() {
+        final CultivationStage stage = CultivationLogicProviders.getCultivationStageFromRegistry(this.cultivationStage);
+
+        for (var statName : stage.getPlayerAttrMap().keySet()) {
+            this.playerAttr.put(statName, this.playerAttr.get(statName) + stage.getPlayerAttr(statName));
+        }
+    }
+
+    public void applyAttributeChangesFromItems() {
+
+    }
+
+    public int getThresholdQi() {
+        return thresholdQi;
+    }
+
+    public void setThresholdQi(int thresholdQi) {
+        this.thresholdQi = thresholdQi;
     }
 }
--- src/main/java/com/failall/cultivation_miracle/event/CultivationCapEvents.java
+++ src/main/java/com/failall/cultivation_miracle/event/CultivationCapEvents.java
@@ -2,10 +2,18 @@ package com.failall.cultivation_miracle.event;
 
 import com.failall.cultivation_miracle.CultivationMiracle;
 import com.failall.cultivation_miracle.capability.cultivation.CultivationProvider;
+import com.failall.cultivation_miracle.cultivation.CultivationRealm;
+import com.failall.cultivation_miracle.cultivation.CultivationStage;
+import com.failall.cultivation_miracle.registry.RegistryCultivationRealms;
+import com.failall.cultivation_miracle.registry.RegistryCultivationStages;
+import net.minecraft.network.chat.Component;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.LogicalSide;
 import net.minecraftforge.fml.common.Mod;
+
+import java.rmi.NoSuchObjectException;
+import java.util.NoSuchElementException;
 
 @Mod.EventBusSubscriber(modid = CultivationMiracle.MOD_ID)
 public class CultivationCapEvents {
@@ -15,8 +23,26 @@ public class CultivationCapEvents {
     public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
         if(event.side == LogicalSide.SERVER) {
             event.player.getCapability(CultivationProvider.PLAYER_QI).ifPresent(cultivator -> {
-                if(cultivator.getCultivationStatus()) {
-                    
+                // Check if threshold QI is reached --> handle rest
+                // Stage section
+                CultivationStage stage = RegistryCultivationStages.DEFERRED_CULTIVATION_STAGES.getEntries().stream().filter(
+                        c -> c.getId() == cultivator.getCultivationStage()
+                ).findFirst().get().orElseThrow(() -> new NoSuchElementException("This cultivation stage does not exist."));
+
+                if (cultivator.getMaxQi() >= cultivator.getThresholdQi() && stage.getStageRankingNumber() <= 7) {
+                    cultivator.rankUpStage();
+                    cultivator.applyAttributeChangesFromStage();
+                }
+
+
+                // Realm section
+                CultivationRealm realm = RegistryCultivationRealms.DEFERRED_CULTIVATION_REALMS.getEntries().stream().filter(
+                        c -> c.getId() == cultivator.getCultivationRealm()
+                ).findFirst().get().orElseThrow(() -> new NoSuchElementException("This cultivation realm does not exist."));
+
+                // Cultivation check & calculation
+                if (cultivator.getCultivationStatus()) {
+
                 }
             });
         }
